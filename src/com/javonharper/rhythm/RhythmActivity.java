1570bf76ebc79f936264eef8d32be859e62cf679
Javon Harper <javon.d.harper@gmail.com>
2013-12-01 19:15:34 -0500

    Make startStop button toggle text

--- src/com/javonharper/rhythm/RhythmActivity.java
+++ src/com/javonharper/rhythm/RhythmActivity.java
@@ -2,12 +2,47 @@ package com.javonharper.rhythm;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RhythmActivity extends Activity {
+	private boolean metronomeActive = false;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_rhythm);
+	}
+
+	public void toggleMetronome(View view) {
+		if (metronomeActive) {
+			changeButtonText("Stop");
+			stopMetronome();
+		} else {
+			changeButtonText("Start");
+			startMetronome();
+		}
+	}
+
+	public void increaseBpm(View view) {
+
+	}
+
+	public void decreaseBpm(View view) {
+
+	}
+
+	private void startMetronome() {
+		metronomeActive = true;
+		
+	}
+
+	private void stopMetronome() {
+		metronomeActive = false;
+	}
+	
+	private void changeButtonText(String text) {
+		Button startStopButton = (Button)findViewById(R.id.start_stop_button);
+		startStopButton.setText(text);
 	}
 }
